fa53072db71be0cb9b37a30f0e9d1687ee8e69f0
Carpdmr <drochman@icloud.com>
2016-01-12 12:01:43 -0800

    Fourth commit

--- Scripts/LoadingData.R
+++ Scripts/LoadingData.R
@@ -7,3 +7,9 @@ if (!file.exists(householdFile)) {
      unzip(downloadFile, overwrite = T, exdir = "./Data")
 }
 hhpc_data <- read.csv("~/OneDrive/Education/Coursera/Classes/Exploratory Data Analysis/Assignment 1/household_power_consumption.txt", sep=";", na.strings="?")
+setwd("~/OneDrive/Education/Coursera/Classes/Exploratory Data Analysis/Assignment 1")
+hhpc_data <- read.csv("~/OneDrive/Education/Coursera/Classes/Exploratory Data Analysis/Assignment 1/household_power_consumption.txt", sep=";", na.strings="?")
+finalData <- hhpc_data[hhpc_data$Date %in% c("1/2/2007","2/2/2007"),]
+settime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(settime, finalData)
+quartz()
